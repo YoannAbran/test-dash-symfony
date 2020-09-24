b5e7ef768bef91b01a5a7099f4b02df7e550e10d
Yoann Abran <y.abran@codeur.online>
2020-09-24 13:58:57 +0200

    test google chart bundle work with querybuilder & findAll()

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+    CMEN\GoogleChartsBundle\CMENGoogleChartsBundle::class => ['all' => true],
 ];
--- src/Controller/LivreController.php
+++ src/Controller/LivreController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use CMEN\GoogleChartsBundle\GoogleCharts\Charts\PieChart;
+use CMEN\GoogleChartsBundle\GoogleCharts\Charts\BarChart;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,54 +16,71 @@ class LivreController extends AbstractController
      * @Route("/livre", name="livre")
      * @Route("/livre/{id}", name="livre_show")
      */
-     public function showGraphData()
- {
-
-    $allVentes = $this->getDoctrine()
+    public function showGraphData()
+    {
+        $allVentes = $this->getDoctrine()
                     ->getRepository(Vente::class)
                     ->findAll();
 
-    $ventes = $this->getDoctrine()
+        $ventes = $this->getDoctrine()
                    ->getRepository(Vente::class)
                    ->getTotalVente();
 
-    $livres = $this->getDoctrine()
+        $livres = $this->getDoctrine()
                    ->getRepository(Livres::class)
                    ->getSumCat();
-    $sumCatYears = $this->getDoctrine()
+        $sumCatYears = $this->getDoctrine()
                    ->getRepository(Livres::class)
                    ->getSumCatYear();
-    $sumCatTots= $this->getDoctrine()
+        $sumCatTots= $this->getDoctrine()
                    ->getRepository(Livres::class)
                    ->getSumCatTotalYear();
 
 
 
-     if (!$ventes && !$livres && $allVentes && $sumCatYears && $sumCatTots) {
-         throw $this->createNotFoundException(
-             'No results Found !! '
-         );
-     }
+        $dataBar = [[ 'Categorie' , 'Prix' ]];
+        foreach ($livres as $livre) {
+            $dataBar[]=array(
+            $livre['categorie'], (int)$livre['sumPrix']);
+        }
+        $bar = new BarChart();
+        $bar->getData()->setArrayToDataTable($dataBar);
 
- return $this->render('test.html.twig',['ventes'=>$ventes, 'livres'=>$livres,'allVentes'=>$allVentes,'sumCatYears'=>$sumCatYears,'sumCatTots'=>$sumCatTots]);
-}
+
+        $data = [[ 'Livre', 'vente' ]];
+        foreach ($allVentes as $allVente) {
+            $data[] = array(
+            $allVente->getNom(), $allVente->getNbreVente()
+        );
+        }
+        $pieChart = new PieChart();
+        $pieChart->getData()->setArrayToDataTable(
+            $data
+        );
+
+        if (!$ventes && !$livres && $allVentes && $sumCatYears && $sumCatTots) {
+            throw $this->createNotFoundException(
+                'No results Found !! '
+            );
+        }
+
+        return $this->render('test.html.twig', ['ventes'=>$ventes, 'livres'=>$livres,'allVentes'=>$allVentes,'sumCatYears'=>$sumCatYears,'sumCatTots'=>$sumCatTots,'piechart'=>$pieChart,'barchart'=>$bar]);
+    }
 
 
-//work but must be in another controler for routes & functiun
-public function show($id){
-  $vente = $this->getDoctrine()
+    //work but must be in another controler for routes & functiun
+    public function show($id)
+    {
+        $vente = $this->getDoctrine()
          ->getRepository(Vente::class)
          ->find($id);
 
-     if (!$vente) {
-         throw $this->createNotFoundException(
-             'No product found for id '.$id
-         );
-     }
+        if (!$vente) {
+            throw $this->createNotFoundException(
+                'No product found for id '.$id
+            );
+        }
 
-     return new Response('Check out this great product: '.$vente->getNom());
-
-
-}
-
+        return new Response('Check out this great product: '.$vente->getNom());
+    }
 }
